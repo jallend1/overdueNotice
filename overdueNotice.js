a0f9ec8d9c872c5e0b7e949f9eb83f05137f2d01
Jason Allen <jallend1@gmail.com>
2024-05-07 13:45:30 -0700

    Copies default template to clipboard if no overdues found just in case

--- overdueNotice.js
+++ overdueNotice.js
@@ -1,8 +1,9 @@
 /* Description: Scrapes the page for any overdue interlibrary loan titles and
 copies an overdue notice letter containing the relevant info to the clipboard. */
 
-let todaysDate = new Date().toLocaleDateString();
+window.focus();
 
+let todaysDate = new Date().toLocaleDateString();
 const overdueTitles = [];
 let overdueText = '';
 let letterCOntent = '';
@@ -20,7 +21,10 @@ lessIntenseAlertDivs.forEach((div) => {
 });
 
 const determineOverdueText = () => {
-    if(overdueTitles.length === 1) {
+    if(overdueTitles.length === 0) {
+        return  `The Interlibrary Loan book "ILLTITLEGOESHERE" is overdue to the library we borrowed it from, and they would like it returned as soon as possible. The King County Library System may be blocked from borrowing from this library system until this item has been returned. We appreciate you returning any overdue interlibrary loan items at your earliest opportunity. This helps ensure that King County Library System will be able to borrow from this library in the future.`;
+    }
+    else if(overdueTitles.length === 1) {
         return  `The Interlibrary Loan book "${overdueTitles[0]}" is overdue to the library we borrowed it from, and they would like it returned as soon as possible. The King County Library System may be blocked from borrowing from this library system until this item has been returned. We appreciate you returning any overdue interlibrary loan items at your earliest opportunity. This helps ensure that King County Library System will be able to borrow from this library in the future.`;
     } else if (overdueTitles.length === 2) {
         return `The Interlibrary Loan books "${overdueTitles[0]}" and "${overdueTitles[1]}" are overdue to the library we borrowed them from, and they would like them returned as soon as possible. The King County Library System may be blocked from borrowing from this library system until these items have been returned. We appreciate you returning any overdue interlibrary loan items at your earliest opportunity. This helps ensure that King County Library System will be able to borrow from this library in the future.`;
@@ -35,18 +39,15 @@ const determineOverdueText = () => {
         });
         return `The Interlibrary Loan books ${overdueText}are overdue to the library we borrowed them from, and they would like them returned as soon as possible. The King County Library System may be blocked from borrowing from this library system until these items have been returned. We appreciate you returning any overdue interlibrary loan items at your earliest opportunity. This helps ensure that King County Library System will be able to borrow from this library in the future.`;
     }
-    else{
-        return null;
-    }
 };  
 
 overdueText = determineOverdueText();
 
-if(overdueText === null) {
-    alert('No overdue interlibrary loan titles found.');
+if(overdueTitles.length === 0) {
+    alert('No overdue interlibrary loan titles found. A letter template was copied to your clipboard.');
 }
-else{
-    letterContent = `
+
+letterContent = `
 King County Library System
 Interlibrary Loan
 960 Newport Way NW * Issaquah, WA 98027 * 425.369.3490 
@@ -60,5 +61,6 @@ ${overdueText}
 Unfortunately, we are not able to issue renewals on interlibrary loan books. If you need more time, you are able to submit a new request once your account is cleared of overdue interlibrary loan titles. This lets us get a copy from a different system, and honor the agreements we made with the libraries that share their collections with us. It also helps to avoid any non-refundable processing fees or replacement costs.
 
 Please do not hesitate to reach out to me if you have any questions. And if you have returned this book since the date above? Please accept our sincerest thanks!`;
-    navigator.clipboard.writeText(letterContent);
-}
+
+navigator.clipboard.writeText(letterContent);
+
